916bcd2d6ae82abec11a0065602fd30ffc1d3337
Odin von Doom <chargingsleipnir@hotmail.com>
2020-04-20 13:51:47 -0400

    Doom lagoon page update

--- root/pages/Games/DoomLagoon.php
+++ root/pages/Games/DoomLagoon.php
@@ -6,17 +6,14 @@
  */
 ?>
 
-<p>
-    An MMO ("mini"-multiplayer online) RPG game I built to dabble in server-authoratative gaming.<br />
-    Up to 3 players can drop into & out-of battles at any time! (Use multiple browsers to try it out!)<br />
-    Allows for local storage or SQL game saves.
-</p>
-
 <p>Play directly here: <a href="https://doomlagoon.herokuapp.com/" class="inlineLink" target="_blank">Doom Lagoon</a></p>
 
 <p>Or play in iframe here:</p>
 <div class="centred">
     <iframe class="subHeaderDispElem" src="https://doomlagoon.herokuapp.com/" width="970" height="610" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen"></iframe>
 </div>
+<p>An MMO ("mini"-multiplayer online) RPG game I built to dabble in server-authoratative gaming.</p>
+<p>Up to 3 players can drop into & out-of battles at any time! (Use multiple browsers to try it out!)</p>
+<p>Allows for local storage or SQL game saves.</p>
 <p>Dev console logs are still active for your interest. (Click a tile, or use the I, J, K, L keys to get data of neighboring cells/tiles)</p>
 <p>(Not yet optimized for IE/Edge)</p>
