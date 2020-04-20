ea18d389cc0fbe84c1dcc173bdf7d59ba3dd2095
Odin von Doom <chargingsleipnir@hotmail.com>
2020-04-20 18:25:57 -0400

    Updated Doom lagoon, well enough optimized for ms edge

--- root/pages/Games/DoomLagoon.php
+++ root/pages/Games/DoomLagoon.php
@@ -16,4 +16,3 @@
 <p>Up to 3 players can drop into & out-of battles at any time! (Use multiple browsers to try it out!)</p>
 <p>Allows for local storage or SQL game saves.</p>
 <p>Dev console logs are still active for your interest. (Click a tile, or use the I, J, K, L keys to get data of neighboring cells/tiles)</p>
-<p>(Not yet optimized for IE/Edge)</p>
